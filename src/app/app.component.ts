e1fd5a19138f916299bb8b6edb352046de61aed5
paul-ventura-git <j.paul.ventura@gmail.com>
2025-04-17 17:10:44 -0500

    Navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LayoutComponent } from "./shared/components/layout/layout.component";
 import { HomeComponent } from "./modules/outer/pages/home/home.component";
+import { HeaderComponent } from './modules/outer/components/header/header.component';
+import { FooterComponent } from "./modules/outer/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LayoutComponent, HomeComponent],
+  imports: [RouterOutlet, LayoutComponent, HomeComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
