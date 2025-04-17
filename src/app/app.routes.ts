3223e103318ccd41282e90c502f26d81c10dca6c
paul-ventura-git <j.paul.ventura@gmail.com>
2025-04-17 16:49:28 -0500

    New components and pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './modules/outer/pages/home/home.component';
+import { GalleryComponent } from './modules/outer/pages/gallery/gallery.component';
+import { ProductDetailsComponent } from './modules/outer/components/product-details/product-details.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
+
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'gallery/:category/:id', component: ProductDetailsComponent },
+  { path: '**', component: NotFoundComponent }
+];
